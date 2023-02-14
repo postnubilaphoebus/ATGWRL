0d5a84ea8c546d4e74e07488173c265217b0a000
Laurids Stockert <63673809+postnubilaphoebus@users.noreply.github.com>
2023-02-14 19:22:52 +0100

    update helper functions and config

--- utils/config.py
+++ utils/config.py
@@ -1,17 +1,26 @@
 import os
 import torch
 
+PAD_ID = 0
+EOS_ID = 1
+BOS_ID = 2
+UNK_ID = 3
+
 # general variables
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-vocab_size = 20_000
-batch_size = 100 # too high batch dimension may overload RAM
+vocab_size = 30_000
+batch_size = 256 # too high batch dimension may overload RAM
+MAX_SENT_LEN = 28 
 
-# vae variables
+# ae variables
 encoder_dim = 100
 decoder_dim = 600
-dropout_prob = 0.2
-vae_learning_rate = 0.001
+latent_dim = 100
+dropout_prob = 0.5
+ae_learning_rate = 5e-4
 word_embedding = 200
 
 # gan variables
-mlp_hidden = 100
+n_layers = 20 
+block_dim = 100
+gan_learning_rate = 1e-4
--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -6,6 +6,26 @@ import regex as re
 import numpy as np
 import time
 import torch
+import truecase
+import random
+import spacy
+import matplotlib.pyplot as plt
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.trainers import BpeTrainer
+from tokenizers.normalizers import NFD, StripAccents
+from tokenizers import normalizers
+
+PAD_ID = 0
+EOS_ID = 1
+BOS_ID = 2
+UNK_ID = 3
+
+MAX_SENT_LEN = 28 # 20 (+ 8 for punctuation)
+MAX_TOKEN_LENGTH = 28
+
+common_nes ={"PERSON": "PERSON_token", "ORG": "ORG_token", "GPE": "GPE_token"}
 
 CONTRACTION_MAP = {"ain't": "is not", "aren't": "are not", "can't": "cannot",
                    "can't've": "cannot have", "'cause": "because", "could've": "could have",
@@ -51,9 +71,161 @@ CONTRACTION_MAP = {"ain't": "is not", "aren't": "are not", "can't": "cannot",
                    "you'll've": "you will have", "you're": "you are", "you've": "you have", "here's": "here is",
                    "here're": "here are", "'d": "had", "'s": "is", "n't": "not"}
 
+def create_bpe_tokenizer(tokenizer_path = "data/tokenizer-toronto.json"):
+    tokenizer_path = "data/tokenizer-toronto.json"
+    cwd = os.getcwd()
+    tokenizer_file = os.path.join(cwd, tokenizer_path)
+
+    if os.path.isfile(tokenizer_file):
+        print("found existing tokenizer")
+        tk = Tokenizer.from_file(tokenizer_file)
+        print("vocab size", tk.get_vocab_size())
+        all_vocab = tk.get_vocab()
+        all_vocab = {k: v for k, v in sorted(all_vocab.items(), key=lambda item: item[1])}
+        print("special tokens", list(all_vocab.items())[:5])
+        return tk
+    
+    else:
+        normalizer = normalizers.Sequence([NFD(), StripAccents()])
+        tokenizer = Tokenizer(BPE())
+        tokenizer.normalizer = normalizer
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = BpeTrainer(special_tokens=["[PAD]", "EOS", "[CLS]", "[UNK]", "[SEP]", "[MASK]"])
+        tokenizer.train(files=["data/bookcorpus_plain.txt"], trainer=trainer)
+        tokenizer.save(tokenizer_file)
+        return tokenizer
+    
+def tokenize_data(tokenizer, plain_path = "data/bookcorpus_plain.txt", ids_path = "data/bookcorpus_ids.txt"):
+
+    cwd = os.getcwd()
+    ids_file = os.path.join(cwd, ids_path)
+
+    if os.path.isfile(ids_file):
+        print("data already tokenized, moving on")
+        return
+    else:
+        if not os.path.isfile(plain_path):
+            print("load dataset ...")
+            dset = load_dataset("bookcorpus")
+    
+            with open("bookcorpus_plain.txt", "w") as f:
+                for element in tqdm(dset['train']):
+                    f.write(element['text'] + "\n")
+                f.close()
+    
+        f = open(plain_path, "r")
+        g = open(ids_path, "w")
+        cnt = 0
+    
+        while True:
+            line = f.readline()
+            line = line.rstrip()
+            if not line:
+                break
+            cnt += 1
+            encoded_line = tk.encode(line)
+            ids = [str(x) for x in encoded_line.ids]
+            if len(ids) > MAX_TOKEN_LENGTH:
+                continue
+            ids_as_str = ' '.join(ids)
+            g.write(ids_as_str + "\n")
+    
+        print("Files created. Found {} sentences", cnt)
+        return
+
+def my_plot(epochs, re_list, kl_div_list, kl_weight_list):
+    re_list = np.array(re_list)
+    kl_div_list = np.array(kl_div_list)
+    kl_weight_list = np.array(kl_weight_list)
+
+    current_directory = os.getcwd()
+    directory = os.path.join(current_directory, r'plotted_losses')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    filename = 'Plotted loss after ' + str(epochs) + 'batches.png'
+    final_directory = os.path.join(directory, filename)
+
+    temp = epochs
+    epochs = []
+    for i in range(temp):
+        epochs.append(i)
+
+    epochs = np.array(epochs)
+    
+    plt.plot(epochs, re_list, label = 'reconstruction error')
+    plt.plot(epochs, kl_div_list, label = 'encoder loss (scaled by 50 for visual)')
+    #plt.plot(epochs, kl_weight_list, label = 'kl weight')
+    
+    #plt.yscale('log')
+    plt.xlabel('Batches')
+    plt.ylabel('Loss')
+    plt.title('Loss plotted over ' + str(temp) + ' batches')
+    plt.legend()
+    plt.savefig(final_directory, dpi=300)
+    plt.close()
+
+def model_usage_by_layer(time_list):
+    total_time = sum(time_list)
+
+    decoding_time = time_list[0] / total_time
+    reparam_time = time_list[1] / total_time
+    code_layer_time = time_list[2] / total_time
+    encoder_plus_dropout_time = time_list[3] / total_time
+    embedding_time = time_list[4] / total_time
+
+    print("Model times by percentage: Embedding {} | Encoder {} | Code Layer {} | Reparam {} | Decoding {}".format(embedding_time, encoder_plus_dropout_time, code_layer_time, reparam_time, decoding_time))
+                   
+def sub_ner_tokens(sentence, nlp):
+    common_nes ={"PERSON": "PERSON_token", "ORG": "ORG_token", "GPE": "GPE_token"}
+    sentence = WhitespaceTokenizer().tokenize(sentence)
+    
+
+    # +1 cause spacy requires joining str with spaces
+    word_lens = [len(x) + 1 for x in sentence] 
+    word_lens[-1] = word_lens[-1] - 1
+
+    character_pos = [sum(word_lens[:i]) for i in range(len(word_lens))]
+    s = ' '.join(sentence)
+
+    # casing required for NER
+    s_upper = truecase.get_true_case(s)
+    doc = nlp(s_upper)
+
+    labels = [ent.label_ for ent in doc.ents]
+    entity_text = [ent.text for ent in doc.ents]
+    label_freq = []
+
+    relevant_idx = []
+    relevant_ne = []
+
+    if labels:
+        for idx, label in enumerate(labels):
+            token = common_nes.get(label)
+            if token:
+                relevant_idx.append(idx)
+                relevant_ne.append(token)
+
+    # return unaltered sentence if no labels found
+    if not relevant_ne:
+        return sentence
+
+    token_start_idx = []
+    for idx, ent in enumerate(doc.ents):
+        if idx in relevant_idx:
+            token_start_idx.append(character_pos.index(ent.start_char))
+
+    temp = list(range(len(relevant_ne)))
+    random.shuffle(temp)
+
+    for idx, ne, rd_idx in zip(token_start_idx, relevant_ne, temp):
+        sentence[idx] = ne + str(rd_idx)
+
+    return sentence
+
 def save_model(epoch, model):
     current_directory = os.getcwd()
-    directory = os.path.join(current_directory, r'saved_vaes')
+    directory = os.path.join(current_directory, r'saved_aes')
     if not os.path.exists(directory):
         os.makedirs(directory)
 
@@ -63,26 +235,40 @@ def save_model(epoch, model):
 
 def real_lengths(unpadded_list):
     sent_lens = [len(i) for i in unpadded_list]
+    sent_lens = [max(min(x, MAX_SENT_LEN), 0) for x in sent_lens]
     return sent_lens
 
 def pad_batch(batch):
     list_len = [len(i) for i in batch]
-    max_len = max(list_len)
+    max_len = MAX_SENT_LEN
     padded_batch = []
     for element in batch:
-        element.extend([0] * (max_len - len(element)))
+        element = element[:max_len]
+        len_dif = max_len - len(element)
+        if len_dif > 0:
+            element = element + [PAD_ID] * len_dif
         padded_batch.append(element)
+    return padded_batch
 
+def pad_batch_and_add_EOS(batch):
+    list_len = [len(i) for i in batch]
+    max_len = MAX_SENT_LEN
+    padded_batch = []
+    for element in batch:
+        element = element + [EOS_ID]
+        if max_len - len(element) > 0:
+            element.extend([PAD_ID] * (max_len - len(element)))
+        element = element[:max_len]
+        padded_batch.append(element)
     return padded_batch
     
 def reformat_decoded_batch(decoded_batch, pad_id):
     decoded_batch = torch.transpose(decoded_batch, 1, 0)
     decoded_batch = decoded_batch.tolist()
-    max_len = len(decoded_batch[0])
+    max_len = MAX_SENT_LEN
     reformatted_batch = []
     for element in decoded_batch:
-        # we include the first PAD as a form of EOS_id
-        first_zero = element.index(pad_id) + 1
+        first_zero = element.index(pad_id)
         element = element[:first_zero]
         if len(element) < max_len:
             element = element + [0] * (max_len - len(element))
@@ -98,18 +284,18 @@ def average_over_nonpadded(accumulated_loss, weights, seqlen_dim):
 def return_weights(real_lens):
     # the sentence and the first padding token
     # weighted as 1 (model rewarded for ending sentence)
-    max_len = max(real_lens)
+    max_len = MAX_SENT_LEN
 
     batch_weights = []
     for length in real_lens:
-        length = length + 1 # add 1 to include pad token as EOS
+        length = length + 1 # add 1 to include EOS token
         weights = [1] * length + (max_len - length) * [0]
         weights = weights[:max_len]
         batch_weights.append(weights)
 
     return batch_weights
 
-def load_vocab(vocab_path):
+def load_vocab(vocab_path, max_size = 20_000):
     count = 0
     vocab = {}
     vocab_file = open(vocab_path, 'r')
@@ -120,6 +306,8 @@ def load_vocab(vocab_path):
             break
         vocab[line] = count
         count += 1
+        if count > max_size:
+            break
     revvocab = {v: k for k, v in vocab.items()}
     return vocab, revvocab
 
@@ -135,38 +323,32 @@ def load_names(names_path):
         name_list.append(line)
     return name_list
 
-def load_data_from_file(data_path):
+def load_data_from_file(data_path, debug = False, retain_validation = True):
+    cwd = os.getcwd()
+    data_path = os.path.join(cwd, data_path)
+    
     print("loading data ids... (est time 3 mins)")
     t1 = time.time()
     data = []
     data_file = open(data_path, 'r')
 
-    debug = True
-
-    if debug:
-        debugging_idx = 0
-        print("debugging load (only 100k)")
-
     while True:
         line = data_file.readline()
-        line = line[1:-2]
         if not line:
             break
+        line = line[2:-2]
         line = line.replace(" ", "")
         line = line.split(",")
         line = [int(x) for x in line]
         data.append(line)
-        if debug:
-            debugging_idx += 1
-            if debugging_idx > 100_000:
-                break
+        
     data_file.close()
     t2 = time.time()
     print("loading data ids took {:.2f} seconds".format(t2-t1))
     return data
 
-def pychant_lookup(word, human_names, american_words):
-    if word == "num000" or word in human_names or american_words.check(word) or american_words.check(word.upper()):
+def pychant_lookup(word, human_names, american_words, special_tokens):
+    if word == "num000" or word in human_names or word in special_tokens or american_words.check(word) or american_words.check(word.upper()):
         return True
     if CONTRACTION_MAP.get(word):
         return True
@@ -174,10 +356,11 @@ def pychant_lookup(word, human_names, american_words):
         
 def load_data_and_create_vocab(dataset = "bookcorpus", word_freq_cutoff = 5, vocab_path = "vocab.txt", names_path = "names.txt"):
 
-    if os.path.isfile(vocab_path) and sum(1 for line in open(vocab_path)) > 20_000:
+    if os.path.isfile(vocab_path) and sum(1 for line in open(vocab_path)) > 10_000:
         x = None
-        print("Found existing vocab file. Size = ", sum(1 for line in open(vocab_path)))
+        print("Found existing vocab file, loading now...")
         vocab, revvocab = load_vocab(vocab_path)
+        print("Vocab size: ", len(vocab))
         return vocab, revvocab, x
     else:
         import enchant
@@ -185,12 +368,19 @@ def load_data_and_create_vocab(dataset = "bookcorpus", word_freq_cutoff = 5, voc
         dset = load_dataset(dataset)
         human_names = load_names(names_path)
         human_names = set(human_names)
-        
+        nlp = spacy.load("en_core_web_sm", disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"])
+
+        special_tokens = []
+
+        for key in common_nes.keys():
+            for i in range(10):
+                special_tokens.append(key + str(i))
+
         word_freqs = {}
         print("creating vocabulary...")
         for element in tqdm(dset['train']):
-            tk = WhitespaceTokenizer().tokenize(element['text'])
-            for word in tk:
+            sentence = sub_ner_tokens(element['text'], nlp)
+            for word in sentence:
                 if word:
                     word = re.sub("^[0-9]+$", "num000", word)
                     if word not in word_freqs:
@@ -198,7 +388,7 @@ def load_data_and_create_vocab(dataset = "bookcorpus", word_freq_cutoff = 5, voc
                     else:
                         word_freqs[word] += 1
 
-        word_freqs = {k: v for k, v in word_freqs.items() if pychant_lookup(k, human_names, american_words)}
+        word_freqs = {k: v for k, v in word_freqs.items() if pychant_lookup(k, human_names, american_words, special_tokens)}
         unfiltered_len = len(word_freqs)
         print("number of words (without filtering)", unfiltered_len)
 
@@ -234,15 +424,17 @@ def prepare_data(dset, vocab, vocab_path = "vocab.txt", data_ids = "bookcorpus_i
         punctuation = ["!", "?", ".", ",", ";", ":", "'", "-"]
         human_names = load_names(names_path)
         human_names = set(human_names)
+        dset = load_dataset("bookcorpus")
+        nlp = spacy.load("en_core_web_sm", disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"])
 
         with open(data_ids, "w") as f, open(data_plain, "w") as g:
             for element in tqdm(dset['train']):
-                tk = WhitespaceTokenizer().tokenize(element['text'])
+                sentence = sub_ner_tokens(element['text'], nlp)
                 line_ids = []
                 line_words = []
                 sentence_length = 0
                 add_sentence = True
-                for word in tk:
+                for word in sentence:
                     if not word:
                         continue
                     word = re.sub("^[0-9]+$", "num000", word)
