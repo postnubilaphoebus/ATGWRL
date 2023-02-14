6c2e12f33686b5cc3bee6e5ebab8e83990fcbcdf
Laurids Stockert <63673809+postnubilaphoebus@users.noreply.github.com>
2023-02-14 19:20:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import torch
 import utils.config as config
-from train_vae import train
+from train_ae import train
+from test_ae import test
+from train_gan import train_gan
 
 def main(*args, **kwargs):
 
     log_dict = train(config)
+    train_gan(config)
 
 if __name__ == "__main__":
     main(config)
